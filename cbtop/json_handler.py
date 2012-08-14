128b0d6ccde5d86b8c2310f24ce52a8d652cf7dc
ronniedada <ronniedada@gmail.com>
2012-08-13 18:46:39 -0700

    proper name formatter for json handler output
    <source>-<ip>.json

--- cbtop/json_handler.py
+++ cbtop/json_handler.py
@@ -11,19 +11,23 @@ class JsonHandler(Handler):
     def __init__(self, filename=""):
         self.filename = filename
 
-    def handle(self, data):
+    def handle(self, source):
         """
         wrap the data into a json doc
         dump to current directory
         """
-        if not data:
+        if not source or \
+            not source.data:
             logging.error("unable to handle : invalid data")
             return False
 
         if not self.filename:
-            self.filename = "unknown.json"
+            self.filename = \
+                source.__class__.__name__ + "-" + \
+                source.server.ip.replace(".", "-") \
+                + ".json"
 
         with open(self.filename, "w") as f:
-            f.write(json.dumps(data))
+            f.write(json.dumps(source.data))
 
         return True
