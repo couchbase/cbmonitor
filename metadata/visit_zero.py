a21bc9d493d32978a37be7e261eedbc94508e51e
Steve Yen <steve.yen@gmail.com>
2012-08-16 12:05:51 -0700

    updated visit_zero.py to emit dict/list JSON

--- metadata/visit_zero.py
+++ metadata/visit_zero.py
@@ -7,52 +7,98 @@ import visit
    values are converted to zeros. By zero'ing out values,
    the metadata becomes more easily diff'able."""
 
+def emit(level, *args):
+    print " " * level + " ".join([str(x) for x in args])
+
 def zero(val):
+    """Converts values into string-ified, JSON-friendly zero values
+       for easy diff'ability."""
     t = type(val)
     if t == int:
-        return 0
+        return "0"
     elif t == float:
-        return 0.0
+        return "0.0"
     elif t == bool:
-        return False
+        return "false"
     elif t == str or t == unicode:
         if val.startswith('/'):
-            return val.split('?')[0]
-    return val
+            val = val.split('?')[0]
+        return '"%s"' % val
+    else:
+        sys.exit("error: unhandled zero val: %s; which has type: %s" % (val, t))
+
+def comma(data, coll, offset=1):
+    """Helper function to add a comma suffix to a line, to meet JSON spec."""
+    if data is None or coll is None:
+        return ""
+    if len(coll) >= len(data) - offset:
+        return ""
+    return ","
 
 def store_zero_fast(root, parents, data, meta, coll,
                     key, val, meta_val, meta_inf, level):
-    if type(coll) == list and key == len(coll):
+    if type(coll) == list:
+        emit(level, zero(val) + comma(data, coll))
         coll.append(zero(val))
     else:
+        emit(level, ('"%s"' % (key)) + ': ' + str(zero(val)) + comma(data, coll))
         coll[key] = zero(val)
     root["run"]["tot_fast"] += 1
 
 def store_zero_slow(root, parents, data, meta, coll,
                     key, val, meta_val, meta_inf, level):
-    if type(coll) == list and key == len(coll):
+    if type(coll) == list:
+        emit(level, zero(val) + comma(data, coll))
         coll.append(zero(val))
     else:
+        emit(level, ('"%s"' % (key)) + ': ' + str(zero(val)) + comma(data, coll))
         coll[key] = zero(val)
     root["run"]["tot_slow"] += 1
 
+def visit_zero_dict(root, parents, data, meta, coll, level=0,
+                    up_key=None, up_data=None, up_coll=None):
+    if type(up_data) == dict:
+        prefix = '"%s": ' % parents[-1]
+    else:
+        prefix = ''
+    suffix = comma(up_data, up_coll, offset=0)
+    emit(level, prefix + "{")
+    visit.visit_dict(root, parents, data, meta, coll, level=level)
+    emit(level, "}" + suffix)
+
+def visit_zero_list(root, parents, data, meta, coll, level=0,
+                    up_key=None, up_data=None, up_coll=None):
+    if type(up_data) == dict:
+        prefix = '"%s": ' % parents[-1]
+    else:
+        prefix = ''
+    suffix = comma(up_data, up_coll, offset=0)
+    emit(level, prefix + "[")
+    visit.visit_list(root, parents, data, meta, coll, level=level)
+    emit(level, "]" + suffix)
+
 def url_zero_before(context, path):
+    print "----------", visit.meta_path(context, path)
     return context, path
 
 def url_zero_after(context, path, root):
-    print "----------", visit.meta_path(context, path)
-    print json.dumps(root["run"]["coll"], sort_keys=True, indent=4)
+    return
 
 if __name__ == '__main__':
     # Override the visit entry funcs to not strip any data.
     visit_entry_funcs = dict(visit.VISIT_ENTRY_FUNCS)
     visit_entry_funcs["strip"] = visit_entry_funcs["default"]
 
+    visit_collection_funcs = {}
+    visit_collection_funcs["<type 'dict'>"] = visit_zero_dict
+    visit_collection_funcs["<type 'list'>"] = visit_zero_list
+
     visit.main("127.0.0.1", 8091, "/pools/default",
                {"fast": store_zero_fast,
                 "slow": store_zero_slow},
                {"url_before": url_zero_before,
                 "url_after": url_zero_after},
+               collection_funcs=visit_collection_funcs,
                entry_funcs=visit_entry_funcs,
                strip_meta=False)
 
