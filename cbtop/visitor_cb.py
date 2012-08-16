f3218b06b594883daf91fe41b563fb2ce1eecbd5
ronniedada <ronniedada@gmail.com>
2012-08-16 11:59:36 -0700

    callback to handle memcached stats
    > get memcached stats
    > dump numeric data to carbon
    > dump original data to a json file

--- cbtop/visitor_cb.py
+++ cbtop/visitor_cb.py
@@ -4,6 +4,12 @@ import logging
 from sys_helper import is_num
 
 from libcarbon.carbon_key import CarbonKey
+from server import Server
+from mc_source import MemcachedSource
+from mc_collector import MemcachedCollector
+from carbon_handler import CarbonHandler
+from json_handler import JsonHandler
+from metadata.data_helper import DataHelper
 
 class VisitorCallback:
     """
@@ -21,7 +27,8 @@ class VisitorCallback:
             return False
 
         if is_num(val):
-            c_key = CarbonKey("ns", "127.0.0.1", key)
+            ip = DataHelper.get_ip(root, parents)
+            c_key = CarbonKey("ns", ip, key)
             VisitorCallback.C_FEEDER.feed(c_key, val)
 
         return True
@@ -30,4 +37,45 @@ class VisitorCallback:
     def store_slow(root, parents, data, meta, coll,
                    key, val, meta_val, meta_inf, level):
         """Store time-series data into slow-changing database"""
+        #TODO
         return True
+
+    @staticmethod
+    def collect_mc_stats(root, parents, data, meta, coll,
+                         key, val, meta_val, meta_inf, level=0):
+        """
+        Collect memcached stats
+        Dump time series data to carbon and save a json snapshot
+        """
+        if not isinstance(val, list):
+            logging.error(
+                "unable to collect mc stats: val must be a list - %s" % val)
+            return False
+
+        logging.info("collecting mc stats from %s" % val)
+
+        for server in val:
+
+            try:
+                ip, port = server.split(":")
+            except (ValueError, AttributeError), e:
+                logging.error(
+                    "unable to collect mc stats from %s : %s" % (server, e))
+                continue
+
+            mc_server = Server(ip)
+
+            # get bucket name from root and parent nodes
+            bucket = DataHelper.get_bucket(root, parents)
+
+            # initialize memcached source
+            mc_source = MemcachedSource(mc_server, bucket)
+
+            # initialize handlers to dump data to carbon and json doc
+            c_handler = CarbonHandler(c_feeder=VisitorCallback.C_FEEDER)
+            j_handler = JsonHandler()
+
+            # collect data from source and emit to handlers
+            mc_coll = MemcachedCollector([mc_source], [c_handler, j_handler])
+            mc_coll.collect()
+            mc_coll.emit()
