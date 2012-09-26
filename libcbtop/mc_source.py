7f7ee294ca7883b0111ef2b12aeb49366a1020fb
ronniedada <ronniedada@gmail.com>
2012-09-26 15:34:41 -0700

    apply filters to store slow and fast mc stats

--- libcbtop/mc_source.py
+++ libcbtop/mc_source.py
@@ -20,10 +20,25 @@ class MemcachedSource(Source):
         self.mc = mc
         self.data = {}
         self.meta = self.get_meta(meta_file)
+        self.slow = {"mc-host": server.ip, "mc-bucket": bucket}
+        self.fast = {"mc-host": server.ip, "mc-bucket": bucket}
 
     def get_meta(self, meta_file):
         with open(meta_file) as f:
             return json.loads(f.read())
+
+    def filter(self, stats):
+        if not stats or not self.meta:
+            logging.error("unable to apply filter: invalid data or meta")
+            return False
+
+        for key in stats:
+            mkey = "mc-%s" % key
+            if mkey in self.meta and "visit" in self.meta[mkey]:
+                if self.meta[mkey]["visit"] == "slow":
+                    self.slow[mkey] = stats[key]
+                elif self.meta[mkey]["visit"] == "fast":
+                    self.fast[mkey] = stats[key]
 
     def connect(self):
         if not self.server or \
@@ -43,10 +58,12 @@ class MemcachedSource(Source):
         self.data = {}
         for key in MemcachedSource.MC_STATS:
             try:
+                stats = self.mc.stats(key)
                 if key != "":
-                    self.data[key] = self.mc.stats(key)
+                    self.data[key] = stats
                 else:
-                    self.data["all"] = self.mc.stats(key)
+                    self.data["all"] = stats
+                self.filter(stats)
             except Exception, e:
                 logging.error("exception for key %s : %s" % (key, e))
 
