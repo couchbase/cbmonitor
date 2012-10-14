d0bc491a21906c81fd4c3ad3ecb63bcdb3a6f02e
Pavel.Paulau <pavel.paulau@gmail.com>
2012-10-14 13:24:01 -0700

    don't import in __init__.py
    lettuce doesn't like it

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^seriesly/(?P<url>.*)$', 'httpproxy.views.proxy'),
-    url(r'^demos/(?P<demo>.*)/$', 'webapp.demos.main'),
-    url(r'^cbdata/$', 'webapp.litmus.cbdata'),
-    url(r'^litmus/$', 'webapp.litmus.litmus'),
+    url(r'^demos/(?P<demo>.*)/$', 'webapp.demos.views.main'),
+    url(r'^cbdata/$', 'webapp.litmus.views.cbdata'),
+    url(r'^litmus/$', 'webapp.litmus.views.litmus'),
 )
