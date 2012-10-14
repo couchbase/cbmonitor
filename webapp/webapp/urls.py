628e8dc3e1d27b7b5441bc8e69e85d74428710f6
Pavel.Paulau <pavel.paulau@gmail.com>
2012-10-13 18:54:11 -0700

    fix url routing
    A couple of important notes:
    1. Currenly we don't have main application/dashboard, therefor
    wildcard urls are not recommended
    2. Url patterns should end with '/' character, otherwise requests
    without slash return 404 status code

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns(
     url(r'^seriesly/(?P<url>.*)$', 'httpproxy.views.proxy'),
     url(r'^demo-flot/$', 'webapp.views.demo_flot', name='demo_flot'),
     url(r'^demo-rickshaw/$', 'webapp.views.demo_rickshaw', name='demo_rickshaw'),
-    url(r'^cbdata$', 'webapp.views.cbdata', name='cbdata'),
-    url(r'^.*$', 'webapp.views.litmus', name='litmus'),
+    url(r'^cbdata/$', 'webapp.views.cbdata', name='cbdata'),
     url(r'^demo-nvd3/$', 'webapp.views.demo_nvd3', name='demo_nvd3'),
     url(r'^demo-cubism/$', 'webapp.views.demo_cubism', name='demo_cubism'),
+    url(r'^litmus/$', 'webapp.views.litmus', name='litmus'),
 )
