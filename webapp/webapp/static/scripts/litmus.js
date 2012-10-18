7d07971fec11563567524642bd16f04b298a137c
ronniedada <ronniedada@gmail.com>
2012-10-18 16:51:22 -0700

    initial commit for litmus dashboard
    - rest api hook-up
    - user defined baseline build for each metric
    - sort, error range and cell coloring
    
    Change-Id: If487ecfc02e82f92528fc9a3f97c150360d8c974
    Reviewed-on: http://review.couchbase.org/21757
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Ronnie Sun <ronnie@couchbase.com>

---
+++ webapp/webapp/static/scripts/litmus.js
@@ -0,0 +1,106 @@
+"use strict";
+
+var oTable;
+var hdrs = [];
+var allBuilds = [];
+var baselines = [];              // TODO: persist
+var ERROR_RANGE = 0.1;                   // TODO: user define
+
+function createSettings() {
+    /*
+     * Create user setting table
+     *
+     * The first two columns are reserved for build infos and timestamps
+     * Actual settings starts from column 3
+     */
+    var buildList = '';
+    var content = '';
+
+    $.each(allBuilds, function(j, build) {
+        buildList += '<option value="' + build + '">';
+        buildList +=  build + '</option>';
+    });
+
+    $.each(hdrs.slice(2), function(i, hdr) {
+        content += '<tr><td>' + hdr.sTitle + '</td>';
+        content += '<td><select>' + buildList + '</select></td></tr>';
+    });
+
+    return content;
+}
+
+function getSettings() {
+    /*
+     * Get user defined settings.
+     */
+    $('#settings table tbody tr').each(function(i) {
+        var build = $(this).find("select option:selected").text();
+        oTable.$('tr').each(function() {
+            var row = oTable.fnGetData(this);
+            if (row[0] === build) {
+                baselines[i] = parseInt(row[2+i]);
+            }
+        });
+    });
+}
+
+function applyErrorRanges() {
+    /*
+     * Apply error range check for each cell.
+     *
+     * If data falls out of the range, turns cell to red.
+     * Accept negative ranges.
+     */
+    oTable.$('tr').each(function() {
+        var row = $(this);
+        var vals = oTable.fnGetData(this);
+        $.each(vals.slice(2), function(j, v) {
+            v = parseInt(v);
+            $(row).find('td').eq(j+2).removeClass();
+            if (!isNaN(baselines[j]) && !isNaN(v)) {
+                var delta = (v - baselines[j]) / baselines[j];
+                if ((delta > ERROR_RANGE && ERROR_RANGE > 0) ||
+                    (delta < ERROR_RANGE && ERROR_RANGE < 0)) {
+                    $(row).find('td').eq(j+2).addClass('red');
+                }
+            }
+        });
+    });
+}
+
+function processData(data) {
+    /*
+     * Process json data coming from the server
+     * Current implementation retrieves info only
+     */
+    $.each(data[0], function(i, v) {
+        hdrs[i] = {'sTitle': v};
+    });
+
+    $.each(data.slice(1), function(i, v) {
+        allBuilds[i] = v[0];
+    });
+
+    return data;
+}
+
+function renderTable(data) {
+    /*
+     * Render table with the given data
+     */
+
+    oTable = $('#litmus table').dataTable({
+        "sDom": 'Tlfrtip',
+        'bProcessing': true,
+        'bPaginate': true,
+        'sPaginationType': 'full_numbers',
+        'aaData': data.slice(1),
+        'aoColumns': hdrs,
+        'aaSorting': [[0, 'desc']],
+        'fnDrawCallback': function() {
+            $('#loading').remove();
+        }
+    }).bind('sort', function () {
+        applyErrorRanges();
+    });
+}
