9029f8a620f868564d6ee121907612a6377e3c17
Pavel.Paulau <pavel.paulau@gmail.com>
2012-10-25 17:50:23 -0700

    allow to edit litmus results in admin console
    Change-Id: I9b41f509397f90e8bd3398f2fbe64abe5b4dc9e6
    Reviewed-on: http://review.couchbase.org/21995
    Reviewed-by: Ronnie Sun <ronnie@couchbase.com>
    Tested-by: Ronnie Sun <ronnie@couchbase.com>

--- webapp/litmus/models.py
+++ webapp/litmus/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib import admin
+
 
 class TestResults(models.Model):
     build = models.CharField(max_length=50)
     timestamp = models.CharField(max_length=20)
     metric = models.CharField(max_length=128)
     value = models.FloatField()
+
+    class Admin:
+        pass
+
+admin.site.register(TestResults)
