93c4646439536ee8e0eab99e3286459b677be760
Pavel.Paulau <pavel.paulau@gmail.com>
2012-12-07 18:01:05 +0300

    add server model
    Change-Id: Ic9b5208b8a241b573dcc92aa82d60e0a7cc98ef5
    Reviewed-on: http://review.couchbase.org/23144
    Reviewed-by: Ronnie Sun <ronnie@couchbase.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- webapp/cbmonitor/models.py
+++ webapp/cbmonitor/models.py
@@ -13,4 +13,24 @@ class Cluster(models.Model):
     class Admin:
         pass
 
+
+class Server(models.Model):
+
+    cluster = models.ForeignKey('Cluster')
+    address = models.CharField(max_length=80)
+    rest_username = models.CharField(max_length=25)
+    rest_password = models.CharField(max_length=50)
+    ssh_username = models.CharField(max_length=25)
+    ssh_password = models.CharField(max_length=50, null=True, blank=True)
+    ssh_key = models.CharField(max_length=4096, null=True, blank=True)
+    description = models.CharField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.address
+
+    class Admin:
+        pass
+
+
 admin.site.register(Cluster)
+admin.site.register(Server)
