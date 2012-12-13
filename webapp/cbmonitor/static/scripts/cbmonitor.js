1a21d31a604d6652334e0eeaa1ad2f5d20a7a532
Pavel.Paulau <pavel.paulau@gmail.com>
2012-12-13 12:17:16 +0300

    implement cluster tree
    Change-Id: Ifc61831872aff008d82a70d53cf9effe059a520b
    Reviewed-on: http://review.couchbase.org/23274
    Reviewed-by: Ronnie Sun <ronnie@couchbase.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- webapp/cbmonitor/static/scripts/cbmonitor.js
+++ webapp/cbmonitor/static/scripts/cbmonitor.js
@@ -1,20 +1,50 @@
 $(document).ready(function(){
-    $(function() {
-        $( "#accordion" ).accordion({
-            heightStyle: "fill"
-        });
-    });
+    var jstree = $("#tree");
+    var renc = $("#renc");
+    var delc = $("#delc");
+    var adds = $("#adds");
 
     $(function() {
-        $("button")
+        $("#accordion").accordion({
+            heightStyle: "fill"
+        });
+        $("input:button")
             .button()
             .css({
                 fontSize: "77%",
-                width: "32%"
+                width: "23%"
             })
-            .click(function( event ) {
-                event.preventDefault();
+            .click(function() {
+                var selected = jstree.jstree("get_selected"),
+                    selected_id = selected.attr("id");
+                switch(this.value) {
+                    case "Add cluster":
+                        jstree.jstree("create", -1, "last", {"attr": {"class": "cluster"}});
+                        break;
+                    case "Add server":
+                        jstree.jstree("create", null, "last", {"attr": {"class": "server"}});
+                        break;
+                    case "Add bucket":
+                        jstree.jstree("create", null, "last", {"attr": {"class": "bucket"}});
+                        break;
+                    case "Edit":
+                        jstree.jstree("rename", selected_id);
+                        break;
+                    case "Delete":
+                        var prnt = $.jstree._reference(selected)._get_parent(selected),
+                            prev = $.jstree._reference(selected)._get_prev(selected);
+                        if (prnt === -1 && prev === false) {
+                            renc.addClass("ui-state-disabled");
+                            delc.addClass("ui-state-disabled");
+                            adds.addClass("ui-state-disabled");
+                        }
+                        jstree.jstree("remove", selected_id);
+                        break;
+                    default:
+                        break;
+                }
             });
+
         $("input:text")
             .button()
             .css({
@@ -22,5 +52,66 @@ $(document).ready(function(){
                 textAlign: "left",
                 cursor : 'text'
             });
+    });
+
+    $(function () {
+        renc.addClass("ui-state-disabled");
+        delc.addClass("ui-state-disabled");
+        adds.addClass("ui-state-disabled");
+
+        jstree.jstree({
+            "themes" : {
+                "theme" : "default",
+                "icons" : false
+            },
+            "crrm": {
+                "move": {
+                    "check_move": function (m) {
+                        var p = this._get_parent(m.o);
+                        if (!p) {
+                            return false;
+                        }
+                        p = p === -1 ? this.get_container() : p;
+                        if (p === m.np) {
+                            return true;
+                        }
+                        return p[0] && m.np[0] && p[0] === m.np[0];
+                    }
+                }
+            },
+            "dnd": {
+                "drop_target": false,
+                "drag_target": false
+            },
+            "plugins": ["themes", "json_data", "dnd", "ui", "crrm"],
+            "json_data": {"data": []}
+        });
+        jstree.on('loaded.jstree', function() {
+            jstree.jstree('open_all');
+        });
+        jstree.bind("select_node.jstree", function (event, data) {
+            var cls = data.rslt.obj.attr("class").split(" ")[0];
+            switch(cls) {
+                case "cluster":
+                    renc.removeClass("ui-state-disabled");
+                    delc.removeClass("ui-state-disabled");
+                    adds.removeClass("ui-state-disabled");
+                    adds.prop("value", "Add server");
+                    break;
+                case "server":
+                    renc.removeClass("ui-state-disabled");
+                    delc.removeClass("ui-state-disabled");
+                    adds.removeClass("ui-state-disabled");
+                    adds.prop("value", "Add bucket");
+                    break;
+                case "bucket":
+                    renc.removeClass("ui-state-disabled");
+                    delc.removeClass("ui-state-disabled");
+                    adds.addClass("ui-state-disabled");
+                    break;
+                default:
+                    break;
+            }
+        });
     });
 });
