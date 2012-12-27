d2d5ba7730becc65cb62eab6995d57a1864766be
pavel-paulau <pavel.paulau@gmail.com>
2012-12-27 16:09:30 +0300

    fix case with null port
    Change-Id: Idb741492d86879b2ba10b9157d15621fe2206691
    Reviewed-on: http://review.couchbase.org/23565
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- webapp/cbmonitor/models.py
+++ webapp/cbmonitor/models.py
@@ -52,7 +52,7 @@ class Bucket(models.Model):
     name = models.CharField(max_length=32, default="default")
     server = models.ForeignKey("Server")
     type = models.ForeignKey("BucketType")
-    port = models.IntegerField(default=11211, blank=True)
+    port = models.IntegerField(default=11211, null=True)
     password = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
