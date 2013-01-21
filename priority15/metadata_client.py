89b1790a5b12a90d03d09280f92f8d068b799a1b
pavel-paulau <pavel.paulau@gmail.com>
2013-01-21 23:38:41 +0300

    add metadata client
    Change-Id: I6225d104548d26301a7aaa3648b15cd427c97b14
    Reviewed-on: http://review.couchbase.org/24109
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

---
+++ priority15/metadata_client.py
@@ -0,0 +1,42 @@
+import requests
+
+
+def post_request(request):
+    def wrapper(*args, **kargs):
+        url, params = request(*args, **kargs)
+        requests.post(url, params)
+    return wrapper
+
+
+class MetadataClient(object):
+
+    def __init__(self, host="127.0.0.1"):
+        self.base_url = "http://{0}:8000/cbmonitor".format(host)
+
+    @post_request
+    def add_cluster(self, name):
+        url = self.base_url + "/add_cluster/"
+        params = {"name": name}
+        return url, params
+
+    @post_request
+    def add_server(self, cluster, address):
+        url = self.base_url + "/add_server/"
+        params = {"cluster": cluster, "address": address}
+        return url, params
+
+    @post_request
+    def add_bucket(self, cluster, name):
+        url = self.base_url + "/add_bucket/"
+        params = {"cluster": cluster, "name": name, "type": "Couchbase"}
+        return url, params
+
+    @post_request
+    def add_metric(self, cluster, name, bucket=None, server=None):
+        url = self.base_url + "/add_metric_or_event/"
+        params = {"type": "metric", "cluster": cluster, "name": name}
+        if server:
+            params["server"] = server
+        if bucket:
+            params["bucket"] = bucket
+        return url, params
