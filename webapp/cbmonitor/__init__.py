618908ffe1fcffd34f6df4b18381914529c36c55
pavel-paulau <pavel.paulau@gmail.com>
2013-02-02 15:41:10 -0800

    enable async writes in sqlite driver
    Change-Id: I15ddac825e2cf379061ab5f0cc51bf1b47a7a62a
    Reviewed-on: http://review.couchbase.org/24374
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

---
+++ webapp/cbmonitor/__init__.py
@@ -0,0 +1,10 @@
+from django.db.backends.signals import connection_created
+
+
+def pragma_synchronous_off(sender, connection, **kwargs):
+    """Disable synchronous writes in sqlite."""
+    if connection.vendor == "sqlite":
+        cursor = connection.cursor()
+        cursor.execute("PRAGMA synchronous=OFF;")
+
+connection_created.connect(pragma_synchronous_off)
