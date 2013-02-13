bdddb931b387a03a73883a05db0307bd475a3ae1
pavel-paulau <pavel.paulau@gmail.com>
2013-02-13 14:49:46 -0800

    add test for ns_server collector
    Change-Id: I79be1e0b6edda294b9718521aff4865e1aa8e2ba
    Reviewed-on: http://review.couchbase.org/24585
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- Makefile
+++ Makefile
@@ -6,8 +6,27 @@ clean: ; \
     rm -fr collectors/cbagent.egg-info collectors/dist collectors/build; \
     rm -f `find . -name *.pyc`
 
-test: ; \
-    buildout -t 120 -q; \
-    ./bin/django test_coverage cbmonitor; \
-    ./bin/pep8 --ignore=E501 collectors cbtop priority15 webapp; \
+buildout: ; \
+    buildout -t 120 -q;
+
+pep8: ; \
+    ./bin/pep8 --ignore=E501 collectors cbtop priority15 webapp
+
+jshint: ; \
     jshint webapp/cbmonitor/static/scripts
+
+test_webapp: ; \
+    ./bin/django test_coverage cbmonitor
+
+start_webapp: ; \
+    ./bin/django syncdb --noinput; \
+    ./bin/django runserver --nothreading --traceback &
+
+stop_webapp: ; \
+    kill -9 `ps -ef | grep django | grep -v grep | awk '{print $$2}'` 2>/dev/null; \
+    rm -fr cbmonitor.db
+
+test_collectors: ; \
+    ./bin/nosetests collectors
+
+test: buildout pep8 jshint test_webapp start_webapp test_collectors stop_webapp;
--- collectors/cbagent/tests.py
+++ collectors/cbagent/tests.py
@@ -2,6 +2,11 @@ import unittest
 from multiprocessing import Process
 
 from cbmock import cbmock
+import requests
+
+
+from cbagent.settings import DefaultSettings
+from cbagent.collectors import NSServer
 
 
 class MockHelper(object):
@@ -13,13 +18,55 @@ class MockHelper(object):
     def __del__(self):
         self.mock.terminate()
 
+    def train_seriesly(self):
+        params = {
+            "path": "/_all_dbs", "response_body": '["cbmonitor"]',
+            "method": "GET", "response_code": "200"
+        }
+        requests.post(url="http://127.0.0.1:8080/", params=params)
+
+    def _submit_sample(self, path, sample):
+        base_path = "collectors/cbagent/fixtures/"
+        params = {"method": "GET", "response_code": 200, "path": path}
+        with open(base_path + sample) as fh:
+            requests.post(url="http://127.0.0.1:8080/", params=params,
+                          files={"response_body": fh})
+
+    def train_couchbase(self):
+        paths = [
+            "/pools/default",
+            "/pools/default/buckets",
+            "/pools/default/buckets/default/nodes",
+            "/pools/default/buckets/default/stats",
+            "/pools/default/buckets/default/statsDirectory",
+            "/pools/default/buckets/default/nodes/127.0.0.1%3A8091/stats"
+        ]
+        samples = [
+            "pools_default.json",
+            "pools_default_buckets.json",
+            "pools_default_buckets_default_nodes.json",
+            "pools_default_buckets_default_stats.json",
+            "pools_default_buckets_default_statsDirectory.json",
+            "pools_default_buckets_default_nodes_127.0.0.1_stats.json"
+        ]
+        test_data = dict(zip(paths, samples))
+        for path, sample in test_data.iteritems():
+            self._submit_sample(path, sample)
+
 
 class CollectorTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.mock = MockHelper()
+        cls.mock.train_seriesly()
+        cls.mock.train_couchbase()
 
     @classmethod
     def tearDownClass(cls):
         del cls.mock
+
+    def test_ns_collector_metadata_update(self):
+        settings = DefaultSettings()
+        ns_collector = NSServer(settings)
+        ns_collector.update_metadata()
