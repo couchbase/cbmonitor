0e111761beeaf3892357992cc1fb79fdf0ded96f
pavel-paulau <pavel.paulau@gmail.com>
2013-02-23 22:18:28 +0300

    remove obsolete code from index view
    Change-Id: I3fb4f7fd23f7237d8b4219d9131ecedb61fc7ba6
    Reviewed-on: http://review.couchbase.org/24808
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- webapp/cbmonitor/views.py
+++ webapp/cbmonitor/views.py
@@ -1,23 +1,5 @@
 from django.shortcuts import render_to_response
 
-import models
-
 
 def index(request):
-    clusters = [c for c in models.Cluster.objects.all()]
-    try:
-        servers = [c for c in models.Server.objects.filter(cluster=clusters[0])]
-    except IndexError:
-        servers = []
-    try:
-        buckets = [b for b in models.Bucket.objects.filter(cluster=clusters[0])]
-    except IndexError:
-        buckets = []
-
-    data = {
-        "clusters": [c.name for c in clusters],
-        "servers": [s.address for s in servers],
-        "buckets": [b.name for b in buckets]
-    }
-
-    return render_to_response("index.jade", data)
+    return render_to_response("index.jade")
