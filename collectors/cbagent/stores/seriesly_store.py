cc4a687764def43a3f71f053ab24269e156698f0
pavel-paulau <pavel.paulau@gmail.com>
2013-04-04 17:24:11 +0300

    log seriesly events
    Change-Id: I2a351436905efd8648ae60417b6c374bc1b2c482
    Reviewed-on: http://review.couchbase.org/25493
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- collectors/cbagent/stores/seriesly_store.py
+++ collectors/cbagent/stores/seriesly_store.py
@@ -1,6 +1,8 @@
 from seriesly import Seriesly
+from seriesly.exceptions import ConnectionError
 
-from store import Store
+from cbagent.stores.store import Store
+from cbagent.logger import logger
 
 
 class SerieslyStore(Store):
@@ -20,9 +22,21 @@ class SerieslyStore(Store):
             db_name += server.replace(".", "")
         return db_name
 
+    def _get_db(self, db_name):
+        try:
+            existing_dbs = self.seriesly.list_dbs()
+        except ConnectionError as e:
+            logger.interrupt("seriesly not available: {0}".format(e))
+        else:
+            if db_name not in existing_dbs:
+                logger.info("Creating new database: {0}".format(db_name))
+                self.seriesly.create_db(db_name)
+            return self.seriesly[db_name]
+
     def append(self, data, cluster=None, server=None, bucket=None,
                collector=None):
-        db_name = self.build_dbname(cluster, server, bucket, collector)
-        if db_name not in self.seriesly.list_dbs():
-            self.seriesly.create_db(db_name)
-        self.seriesly[db_name].append(data)
+            db_name = self.build_dbname(cluster, server, bucket, collector)
+            db = self._get_db(db_name)
+
+            logger.info("Appending data to: {0}".format(db_name))
+            db.append(data)
