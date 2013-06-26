7a5167635e41163bc35221a675a62c82feec1431
pavel-paulau <pavel.paulau@gmail.com>
2013-06-26 13:19:56 +0300

    clean up setup script
    Change-Id: Ibd0b38170a15bd8cd54c0250fb7197dced9269b1
    Reviewed-on: http://review.couchbase.org/27140
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- collectors/setup.py
+++ collectors/setup.py
@@ -1,33 +1,17 @@
 from setuptools import setup
 
-version = '0.3'
+version = '0.5'
 
 setup(
     name='cbagent',
     version=version,
-    url="https://github.com/couchbase/cbmonitor",
-    classifiers=[
-        "Intended Audience :: Developers",
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        "Operating System :: POSIX",
-        "License :: OSI Approved :: Apache Software License",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Terminals'
-    ],
-    author="Couchbase",
-    author_email="admin@couchbase.com",
-    license="Apache Software License",
+    author='Couchbase',
+    license='Apache Software License',
     packages=[
-        "cbagent",
-        "cbagent.cli",
-        "cbagent.collectors",
-        "cbagent.collectors.libstats"
-    ],
-    py_modules=[
-        "stores",
+        'cbagent',
+        'cbagent.cli',
+        'cbagent.collectors',
+        'cbagent.collectors.libstats'
     ],
     entry_points={
         'console_scripts': [
@@ -38,12 +22,12 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        'requests==1.2.0',
-        'seriesly==0.5.2',
-        'fabric==1.6.0',
         'argparse==1.2.1',
         'eventlet==0.12.1',
+        'fabric==1.6.0',
+        'logger==1.1',
+        'requests==1.2.0',
+        'seriesly==0.5.2',
         'ujson==1.23',
-        'logger==1.1'
     ],
 )
