3be6a631f2f36a18191f71329a3b7ba3f32151c0
pavel-paulau <pavel.paulau@gmail.com>
2013-08-27 19:18:41 +0300

    add ep_num_value_ejects to base report
    Change-Id: I83d8e7a6cd5b71e269fa6b1f162839badbc7a53d
    Reviewed-on: http://review.couchbase.org/28601
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- webapp/cbmonitor/reports.py
+++ webapp/cbmonitor/reports.py
@@ -32,6 +32,7 @@ class BaseReport(object):
             "mem_used",
             "vb_active_resident_items_ratio",
             "vb_replica_resident_items_ratio",
+            "ep_num_value_ejects",
             "disk_write_queue",
             "ep_cache_miss_rate",
             "ep_bg_fetched",
