07dac13ecb347f13b64d0796a645f3b4fe5fdd10
pavel-paulau <pavel.paulau@gmail.com>
2013-12-02 16:52:07 -0800

    use pandas for time series manipulations
    Change-Id: Ia34a0811f4bf94fc80cf631dd1d48abd5635388a
    Reviewed-on: http://review.couchbase.org/30770
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>

--- webapp/cbmonitor/plotter.py
+++ webapp/cbmonitor/plotter.py
@@ -1,4 +1,3 @@
-import math
 import os
 import re
 from calendar import timegm
@@ -24,6 +23,7 @@ matplotlib.rcParams.update({"legend.frameon": True})
 import matplotlib.pyplot as plt
 
 import numpy as np
+import pandas as pd
 from cbagent.stores import SerieslyStore
 from django.conf import settings
 from eventlet import GreenPool
@@ -45,7 +45,7 @@ class Colors(object):
 
 
 # Defined externally in order to be pickled
-def save_png(filename, timestamps, values, ylabel, labels, histogram):
+def save_png(filename, series, ylabel, labels, histogram):
     fig = plt.figure(figsize=(4.66, 2.625))
 
     colors = Colors()
@@ -69,15 +69,14 @@ def save_png(filename, timestamps, values, ylabel, labels, histogram):
             percentiles = range(1, 100)
             x = percentiles
             plt.xlim(0, 100)
-        for i, v in enumerate(values):
-            data = sorted(v)
-            y = np.percentile(data, percentiles)
+        for i, s in enumerate(series):
+            y = np.percentile(s.values, percentiles)
             ax.bar(x, y, linewidth=0.0, label=labels[i],
                    width=width.next(), align=align.next(), color=colors.next())
     else:
         ax.set_xlabel("Time elapsed, sec")
-        for i, timestamp in enumerate(timestamps):
-            ax.plot(timestamp, values[i], label=labels[i], color=colors.next())
+        for i, s in enumerate(series):
+            ax.plot(s.index, s.values, label=labels[i], color=colors.next())
         ymin, ymax = ax.get_ylim()
         plt.ylim(ymin=0, ymax=max(1, ymax * 1.05))
     legend = ax.legend()
@@ -102,7 +101,7 @@ class Plotter(object):
     def __del__(self):
         self.mp_pool.close()
 
-    def get_data(self, snapshot, cluster, server, bucket, metric, collector):
+    def get_series(self, snapshot, cluster, server, bucket, metric, collector):
         # Query data using metric as key
         query_params = {"ptr": "/{0}".format(metric), "reducer": "avg",
                         "group": 5000}
@@ -115,23 +114,18 @@ class Plotter(object):
         try:
             response = self.db[db_name].query(query_params)
         except NotExistingDatabase:
-            return None, None
+            return None
 
-        # Convert data and generate sorted lists of timestamps and values
-        timestamps = list()
-        values = list()
+        # Create time series
         data = dict((k, v[0]) for k, v in response.iteritems())
-        for timestamp, value in sorted(data.iteritems()):
-            timestamps.append(int(timestamp))
-            values.append(value or 0)
+        series = pd.Series(data)
+        series.index = series.index.values - series.index.values.min()
+        series.index = series.index.values / 1000  # ms -> s
 
-        # Substract first timestamp; convert to seconds
-        timestamps = [(key - timestamps[0]) / 1000 for key in timestamps]
-
-        if set(values) == set([0]) and metric in NON_ZERO_VALUES:
-            return None, None
+        if metric in NON_ZERO_VALUES and (series == 0).all():
+            return None
         else:
-            return timestamps, values
+            return series
 
     def generate_png_meta(self, snapshot, cluster, server, bucket, metric):
         metric = metric.replace("/", "_")
@@ -170,11 +164,10 @@ class Plotter(object):
         for sub_metric, snapshot in meta:
             cluster, server, bucket, name, collector = self.extract_meta(
                 sub_metric)
-            timestamps, values = self.get_data(snapshot, cluster, server,
-                                               bucket, name, collector)
-            if timestamps and values:
-                merge["timestamps"].append(timestamps)
-                merge["values"].append(values)
+            series = self.get_series(snapshot, cluster, server, bucket, name,
+                                     collector)
+            if series:
+                merge["series"].append(series)
                 if snapshot.name == "all_data":
                     merge["labels"].append(cluster)
                 else:
@@ -183,14 +176,13 @@ class Plotter(object):
         title, url, filename = self.generate_png_meta(snapshot, merge_cluster,
                                                       server, bucket, name)
 
-        return merge["timestamps"], merge["values"], merge["labels"],\
-            title, filename, url
+        return merge["series"], merge["labels"], title, filename, url
 
     def plot(self, metrics):
         apply_results = list()
         for data in self.eventlet_pool.imap(self.extract, metrics):
-            timestamps, values, labels, title, filename, url = data
-            if timestamps and values:
+            series, labels, title, filename, url = data
+            if series:
                 ylabel = LABELS.get(title.split()[-1], title.split()[-1])
                 suffixes = ['']
                 if title.split()[-1] in HISTOGRAMS:
@@ -202,7 +194,7 @@ class Plotter(object):
                         apply_results.append(self.mp_pool.apply_async(
                             save_png,
                             args=(filename.format(suffix=suffix),
-                                  timestamps, values, ylabel, labels, suffix)
+                                  series, ylabel, labels, suffix)
                         ))
                 for suffix in suffixes:
                     self.urls.append([title, url.format(suffix=suffix)])
