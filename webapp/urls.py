5105e8d7359d062466260ab277f8632483eba44a
pavel-paulau <pavel.paulau@gmail.com>
2013-12-13 10:53:03 -0800

    fix favicon redirect
    Change-Id: Ic3e7a3d3fccaffa70ce6d3dd6a7429842f340112
    Reviewed-on: http://review.couchbase.org/31061
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
@@ -12,7 +13,7 @@ urlpatterns = patterns(
 )
 urlpatterns += patterns(
     '',
-    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^seriesly/(?P<url>.*)$', 'httpproxy.views.proxy'),
