4f996e5b637a23f9450a5bd54e47ef76576dbc1a
pavel-paulau <pavel.paulau@gmail.com>
2014-06-26 21:32:39 -0700

    add ep_upr_replica_items_remaining to reports
    Change-Id: I0e98864f491ce68959ca16142ae1515ae38a636e
    Reviewed-on: http://review.couchbase.org/38862
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- webapp/cbmonitor/plotter/constants.py
+++ webapp/cbmonitor/plotter/constants.py
@@ -13,6 +13,7 @@ LABELS = {
     "vb_active_resident_items_ratio": "Active docs resident, %",
     "vb_replica_resident_items_ratio": "Replica docs resident, %",
     "ep_num_value_ejects": "Ejections per sec",
+    "ep_upr_replica_items_remaining": "UPR replication backlog",
     "disk_write_queue": "Disk write queue, items",
     "ep_cache_miss_rate": "Cache miss ratio, %",
     "ep_bg_fetched": "Disk reads per sec",
