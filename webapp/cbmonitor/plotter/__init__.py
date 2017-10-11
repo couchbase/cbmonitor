couchbase/cbmonitor

commit c49f155903b8e273aa66f49f160ca2a591c52740

Replace Seriesly with perfdb
Time it takes to generate 5h report with frequent sampling:
* Before: 15-16 minutes
* After: 20-30 seconds

Change-Id: I9149fef7a198e86f726313e825d8e14e1363751c
Reviewed-on: http://review.couchbase.org/81935
Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

webapp/cbmonitor/plotter/__init__.py
@@ -1,18 +1,15 @@
-import dateutil.parser
 import os
-import re
-from collections import defaultdict
 from itertools import cycle
-from multiprocessing import Pool, cpu_count
 
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 import numpy as np
 import pandas as pd
+import requests
 from django.conf import settings
-from eventlet import GreenPool
 
-from cbmonitor.helpers import SerieslyHandler
 from cbmonitor.plotter import constants
 from cbmonitor.plotter.reports import Report
 
@@ -25,7 +22,6 @@ matplotlib.rcParams.update({
     'grid.linestyle': 'dotted',
     'grid.linewidth': 0.5,
     'legend.fancybox': True,
-    'legend.loc': 'upper right',
     'legend.markerscale': 2,
     'legend.numpoints': 1,
     'lines.antialiased': False,
@@ -42,14 +38,14 @@ matplotlib.rcParams.update({
 })
 
 
-def plot_as_png(filename, series, labels, colors, ylabel, chart_id, rebalances):
+def plot_as_png(filename, series, labels, colors, ylabel, chart, rebalances):
     """Primary routine that serves as plot selector. The function and all
     sub-functions are defined externally in order to be pickled."""
     fig = plt.figure(figsize=(4.66, 2.625))
     ax = init_ax(fig)
 
-    if chart_id in ("_lt90", "_gt80", "_histo"):
-        plot_percentiles(ax, series, labels, colors, ylabel, chart_id)
+    if chart in ("_lt90", "_gt80", "_histo"):
+        plot_percentiles(ax, series, labels, colors, ylabel, chart)
     else:
         plot_time_series(ax, series, labels, colors, ylabel)
         highlight_rebalance(rebalances, colors)
@@ -80,7 +76,7 @@ def plot_time_series(ax, series, labels, colors, ylabel=None):
     plt.ylim(ymin=0, ymax=max(1, ymax * 1.05))
 
 
-def plot_percentiles(ax, series, labels, colors, ylabel, chart_id):
+def plot_percentiles(ax, series, labels, colors, ylabel, chart):
     """Bar plot with 3 possible percentile ranges and sub-ranges:
     -- 1 to 99 (linear)
     -- 1 to 89 (linear)
@@ -90,11 +86,11 @@ def plot_percentiles(ax, series, labels, colors, ylabel, chart_id):
     ax.set_xlabel("Percentile")
     width = cycle((0.6, 0.4))
     align = cycle(("edge", "center"))
-    if chart_id == "_lt90":
+    if chart == "_lt90":
         percentiles = range(1, 90)
         x = percentiles
         plt.xlim(0, 90)
-    elif chart_id == "_gt80":
+    elif chart == "_gt80":
         percentiles = (80, 85, 90, 95, 97.5, 99, 99.9, 99.99, 99.999)
         x = range(len(percentiles))
         plt.xticks(x, percentiles)
@@ -109,14 +105,90 @@ def plot_percentiles(ax, series, labels, colors, ylabel, chart_id):
 
 
 def highlight_rebalance(rebalances, colors):
-    """Transparent span that highlights rebalance start and end. Makes sense
-    only for similar reports (e.g., rebalance+views vs. rebalance+views).
-    Otherwise disorder in colors may happen."""
+    """Add a transparent vertical span that highlights rebalance start and end.
+    """
     for (start, end), color in zip(rebalances, colors):
         plt.axvspan(start, end, facecolor=color, alpha=0.1, linewidth=0.5)
 
 
-class Colors(object):
+def generate_title(observable):
+    """Generate user-friendly chart title.
+
+    The title is generated using this format:
+
+        [server/bucket] metric.
+    """
+    metric = observable.name.replace("/", "_")
+    if observable.bucket:
+        return "[{}] {}".format(observable.bucket, metric)
+    elif observable.server:
+        return "[{}] {}".format(observable.server, metric)
+    elif observable.index and "." in observable.index:
+        name = observable.index.split(".")
+        return "[{}] [{}] {}".format(name[0], name[1], metric)
+    else:
+        return metric
+
+
+def generate_paths(clusters, metric, suffix):
+    """Generate file name and URL using the unique attributes."""
+    filename = "{}{}{}.png".format(''.join(clusters), metric, suffix)
+
+    media_url = settings.MEDIA_URL + filename
+    media_path = os.path.join(settings.MEDIA_ROOT, filename)
+
+    return media_url, media_path
+
+
+def build_dbname(cluster, server, bucket, index, collector):
+    """Each seriesly db name is built from observable object attributes."""
+    db_name = (collector or "") + cluster + (bucket or "") + (index or "") + (server or "")
+    for char in "[]/\;.,><&*:%=+@!#^()|?^'\"":
+        db_name = db_name.replace(char, "")
+    return db_name
+
+
+def generate_series(data):
+    """Convert array of data points to Pandas series.
+
+    perfdb returns data in the following format:
+
+        [[timestamp, measurement], [timestamp, measurement], ...]
+
+    The new series will create index based on timestamp. Measurements are used
+    as values.
+
+    The function also normalizes and sort data.
+    """
+    series = pd.Series(index=[d[0] for d in data], data=[d[1] for d in data])
+
+    # Subtract the smallest timestamp value so that series starts from 0.
+    starting_point = series.index.values.min()
+    series.rename(lambda timestamp: timestamp - starting_point, inplace=True)
+
+    # Convert milliseconds to seconds
+    series.rename(lambda timestamp: timestamp / 10 ** 3, inplace=True)
+
+    # Return data sorted by timestamp
+    return series.sort_index()
+
+
+def is_all_zeroes(metric, series):
+    """Check if series should be skipped because it contains only zero values.
+    """
+    return metric in constants.NON_ZERO_VALUES and (series == 0).all()
+
+
+def generate_chart_types(metric):
+    charts = ["scatter"]
+    if metric in constants.HISTOGRAMS:
+        charts += ["_histo"]
+    if metric in constants.ZOOM_HISTOGRAMS:
+        charts += ["_lt90", "_gt80"]
+    return charts
+
+
+class Palette:
 
     def __init__(self):
         self.cycle = cycle(constants.PALETTE)
@@ -125,127 +197,98 @@ class Colors(object):
         return self.cycle.next()
 
 
-class Plotter(object):
+class DataClient:
 
-    """Plotter helper that reads data from seriesly database and generates
-    handy charts with url/filesystem meta information."""
+    def __init__(self, host='127.0.0.1', port=8080):
+        self.session = requests.Session()
+        self.base_url = 'http://{}:{}/{{db}}/{{metric}}'.format(host, port)
+
+    def get(self, db, metric):
+        url = self.base_url.format(db=db, metric=metric)
+
+        response = self.session.get(url)
+        if response.status_code == 200:
+            return response.json()
+
+
+class Plotter:
 
     def __init__(self):
-        self.urls = list()  # The only thing that caller (view) needs
-
-        self.eventlet_pool = GreenPool()  # for seriesly requests
-        self.mp_pool = Pool(cpu_count())  # for plotting
-
-        self.seriesly = SerieslyHandler()
-
-    def __del__(self):
-        self.mp_pool.close()
-
-    @staticmethod
-    def generate_title(observable):
-        """[server/bucket] metric"""
-        metric = observable.name.replace("/", "_")
-        if observable.bucket:
-            return "[{}] {}".format(observable.bucket, metric)
-        elif observable.server:
-            return "[{}] {}".format(observable.server, metric)
-        elif observable.index and "." in observable.index:
-            name = observable.index.split(".")
-            return "[{}] [{}] {}".format(name[0], name[1], metric)
-        else:
-            return metric
-
-    def generate_png_meta(self, snapshot, cluster, title):
-        """Generate output filenames and URLs based on object attributes."""
-        filename = "".join((snapshot, cluster, title))
-        filename = re.sub(r"[\[\]/\\:\*\?\"<>\|& ]", "", filename)
-        filename += "{suffix}.png"
-
-        media_url = settings.MEDIA_URL + filename
-        media_path = os.path.join(settings.MEDIA_ROOT, filename)
-        return media_url, media_path
-
-    def get_series(self, metric, data):
-        """Convert raw data to Pandas time series."""
-        series = pd.Series(data)
-        series.dropna()  # otherwise it may break kde
-        if metric in constants.NON_ZERO_VALUES and (series == 0).all():
-            return None
-        series.rename(lambda x: dateutil.parser.parse(x), inplace=True)
-        series.rename(lambda x: int(x.strftime('%s')), inplace=True)
-        series.rename(lambda x: x - series.index.values.min(), inplace=True)
-        return series
-
-    def extract(self, observables, skip_df=False):
-        """Top-level abstraction for data and metadata extraction."""
-        merge = defaultdict(list)
-        title = ""
-        colors = Colors()
-        for observable in observables:
-            color = colors.next()
-            if observable:
-                data = self.seriesly.query_data(observable)
-                if data:
-                    series = self.get_series(metric=observable.name, data=data)
-                    if series is not None:
-                        merge["series"].append(series)
-                        merge["labels"].append(observable.snapshot.name)
-                        merge["colors"].append(color)
-                        merge["clusters"].append(observable.snapshot.cluster.name)
-                        merge["snapshots"].append(observable.snapshot.name)
-                        title = self.generate_title(observable)
-
-        url, fname = self.generate_png_meta("".join(merge["snapshots"]),
-                                            "".join(merge["clusters"]),
-                                            title)
-
-        return merge["series"], merge["labels"], merge["colors"], title, fname, url
+        self.data_client = DataClient()
 
     def detect_rebalance(self, observables):
-        """Check first observable object which is expected to be rebalance
-        progress characteristic."""
+        if observables[0].name != "rebalance_progress":
+            return []
+
+        _series = []
+        for observable in observables:
+            series = self.get_series(observable)
+            if series is not None and not (series == 0).all():
+                _series.append(series)
+            else:
+                return []
+
         rebalances = []
-        if observables[0] and observables[0].name == "rebalance_progress":
-            series, _, _, _, _, _ = self.extract(observables, skip_df=True)
-            for s in series:
-                s = s.dropna()
-                if (s == 0).all():
-                    return []
-                rebalance = s[s > 0]
-                rebalances.append((rebalance.index[0], rebalance.index[-1]))
+        for series in _series:
+            series = series[series > 0]  # Filter rebalance progress
+            rebalances.append([series.index.min, series.index.max])
         return rebalances
 
+    def get_series(self, observable):
+        db = build_dbname(observable.cluster,
+                          observable.server,
+                          observable.bucket,
+                          observable.index,
+                          observable.collector)
+        data = self.data_client.get(db, observable.name)
+        if data:
+            return generate_series(data)
+
+    def generate_chart_data(self, observables):
+        palette = Palette()
+
+        _series = []
+        _clusters = []
+        _colors = []
+
+        for observable in observables:
+            color = palette.next()
+            series = self.get_series(observable)
+            if series is not None and not is_all_zeroes(observable.name, series):
+                _series.append(series)
+                _colors.append(color)
+                _clusters.append(observable.cluster)
+
+        return _series, _clusters, _colors
+
     def plot(self, snapshots):
-        """End-point method that orchestrates concurrent extraction and
-        plotting."""
-        observables = Report(snapshots)()
+        report = Report(snapshots).get_report()
 
-        rebalances = self.detect_rebalance(observables[0])
+        rebalances = self.detect_rebalance(report[0])
 
-        # Asynchronously extract data
-        apply_results = list()
-        for data in self.eventlet_pool.imap(self.extract, observables):
-            series, labels, colors, title, filename, url = data
-            if series:
-                metric = title.split()[-1]
-                ylabel = constants.LABELS.get(metric, metric)
+        images = []
+        for observables in report:
+            series, clusters, colors = self.generate_chart_data(observables)
+            if not series:  # Bad or missing data
+                continue
 
-                chart_ids = [""]
-                if metric in constants.HISTOGRAMS:
-                    chart_ids += ["_histo"]
-                if metric in constants.ZOOM_HISTOGRAMS:
-                    chart_ids += ["_lt90", "_gt80"]
+            metric = observables[0].name
+            title = generate_title(observables[0])
+            ylabel = constants.LABELS.get(metric, metric)
 
-                for chart_id in chart_ids:
-                    fname = filename.format(suffix=chart_id)
-                    if not os.path.exists(fname):
-                        apply_results.append(self.mp_pool.apply_async(
-                            plot_as_png,
-                            args=(fname,
-                                  series, labels, colors, ylabel, chart_id,
-                                  rebalances)
-                        ))
-                    self.urls.append([title, url.format(suffix=chart_id)])
-        # Plot all charts in parallel
-        for result in apply_results:
-            result.get()
+            for chart in generate_chart_types(metric):
+                url, filename = generate_paths(clusters=clusters,
+                                               metric=metric,
+                                               suffix=chart)
+
+                images.append([title, url])
+
+                if not os.path.exists(filename):  # Try cache
+                    plot_as_png(filename=filename,
+                                series=series,
+                                labels=clusters,
+                                colors=colors,
+                                ylabel=ylabel,
+                                chart=chart,
+                                rebalances=rebalances)
+        return images

.flake8
@@ -1,3 +1,3 @@
 [flake8]
-ignore = E501
+ignore = E402,E501
 exclude = webapp/wsgi.py

webapp/cbmonitor/helpers.py
@@ -1,45 +1,0 @@
-import seriesly
-
-
-class SerieslyHandler(object):
-
-    """Simple handler for data stored in seriesly database."""
-
-    MIN_GROUP_INTERVAL = 1000  # 1 sec
-    MAX_GROUP_INTERVAL = 5000  # 5 secs
-    GROUPING_THRESHOLD = 14400  # start grouping when snapshot is longer than 4h
-
-    def __init__(self):
-        self.db = seriesly.Seriesly()
-        self.all_dbs = self.db.list_dbs()
-
-    @staticmethod
-    def build_dbname(cluster, server, bucket, index, collector):
-        """Each seriesly db name is built from observable object attributes."""
-        db_name = (collector or "") + cluster + (bucket or "") + (index or "") + (server or "")
-        for char in "[]/\;.,><&*:%=+@!#^()|?^'\"":
-            db_name = db_name.replace(char, "")
-        return db_name
-
-    def query_data(self, observable):
-        """Read data from seriesly database."""
-        db_name = self.build_dbname(observable.snapshot.cluster.name,
-                                    observable.server, observable.bucket,
-                                    observable.index, observable.collector)
-        if db_name in self.all_dbs:
-            try:
-                raw_data = self.db[db_name].get_all()
-                return {k: v[observable.name] for k, v in raw_data.items()
-                        if observable.name in v}
-            except seriesly.exceptions.ConnectionError:
-                return
-
-    def query_raw_data(self, db_name, name):
-        params = {"ptr": "/{}".format(name), "reducer": "avg", "group": 5000}
-        if db_name in self.all_dbs:
-            try:
-                return self.db[db_name].query(params)
-            except seriesly.exceptions.ConnectionError:
-                return {}
-        else:
-            return {}

webapp/cbmonitor/plotter/constants.py
@@ -1,12 +1,12 @@
 LABELS = {
     "rebalance_progress": "Rebalance progress, %",
-    "ops": "Ops per sec",
-    "cmd_get": "GET ops per sec",
-    "cmd_set": "SET ops per sec",
-    "delete_hits": "DELETE ops per sec",
-    "cas_hits": "CAS ops per sec",
+    "ops": "Ops per second",
+    "cmd_get": "GET ops per second",
+    "cmd_set": "SET ops per second",
+    "delete_hits": "DELETE ops per second",
+    "cas_hits": "CAS ops per second",
     "curr_connections": "Connections",
-    "hibernated_waked": "Streaming requests wakeups per sec",
+    "hibernated_waked": "Streaming requests wakeups per second",
     "curr_items": "Active items",
     "vb_replica_curr_items": "Replica items",
     "vb_pending_curr_items": "Pending items",
@@ -14,25 +14,25 @@ LABELS = {
     "ep_meta_data_memory": "Metadata in RAM, bytes",
     "vb_active_resident_items_ratio": "Active docs resident, %",
     "vb_replica_resident_items_ratio": "Replica docs resident, %",
-    "ep_num_value_ejects": "Ejections per sec",
-    "ep_dcp_2i_items_sent": "DCP drain rate (2i), items/sec",
+    "ep_num_value_ejects": "Ejections per second",
+    "ep_dcp_2i_items_sent": "DCP drain rate (2i), items/second",
     "ep_dcp_2i_items_remaining": "DCP backlog (2i), items",
     "ep_dcp_replica_items_remaining": "DCP backlog (replica), items",
-    "ep_dcp_replica_items_sent": "DCP drain rate (replica), items/sec",
-    "ep_dcp_replica_total_bytes": "DCP drain rate (replica), bytes/sec",
+    "ep_dcp_replica_items_sent": "DCP drain rate (replica), items/second",
+    "ep_dcp_replica_total_bytes": "DCP drain rate (replica), bytes/second",
     "ep_dcp_other_items_remaining": "DCP backlog (other), items",
-    "ep_dcp_other_items_sent": "DCP drain rate (other), items/sec",
-    "ep_dcp_other_total_bytes": "DCP drain rate (other), bytes/sec",
+    "ep_dcp_other_items_sent": "DCP drain rate (other), items/second",
+    "ep_dcp_other_total_bytes": "DCP drain rate (other), bytes/second",
     "disk_write_queue": "Disk write queue, items",
     "ep_cache_miss_rate": "Cache miss ratio, %",
-    "ep_bg_fetched": "Disk reads per sec",
-    "ep_ops_create": "Disk creates per sec",
-    "ep_ops_update": "Disk updates per sec",
-    "ep_diskqueue_drain": "Drain rate, items/sec",
+    "ep_bg_fetched": "Disk reads per second",
+    "ep_ops_create": "Disk creates per second",
+    "ep_ops_update": "Disk updates per second",
+    "ep_diskqueue_drain": "Drain rate, items/second",
     "avg_bg_wait_time": "BgFetcher wait time, us",
     "avg_disk_commit_time": "Disk commit time, s",
     "avg_disk_update_time": "Disk update time, us",
-    "vb_avg_total_queue_age": "Average age of all items in the disk write queue, sec",
+    "vb_avg_total_queue_age": "Average age of all items in the disk write queue, second",
     "couch_docs_data_size": "Docs data size, bytes",
     "couch_docs_actual_disk_size": "Docs total disk size, bytes",
     "couch_docs_fragmentation": "Docs fragmentation, %",
@@ -43,16 +43,16 @@ LABELS = {
     "docs_filtered": "Number of mutations per second that have been filtered out",
     "docs_failed_cr_source": "Number of mutations that failed conflict resolution",
     "rate_replicated": "Number of replicated mutations per second",
-    "bandwidth_usage": "Rate of replication, bytes/sec",
+    "bandwidth_usage": "Rate of replication, bytes/second",
     "rate_doc_opt_repd": "Number of optimistically replicated mutations per second",
     "rate_doc_checks": "Number of doc checks per second",
     "wtavg_meta_latency": "Latency of sending getMeta and waiting for conflict solution, ms",
     "wtavg_docs_latency": "Latency of sending replicated mutations to remote cluster, ms",
-    "xdc_ops": "Total XDCR operations per sec",
-    "ep_num_ops_get_meta": "Metadata reads per sec",
-    "ep_num_ops_set_meta": "Metadata sets per sec",
-    "ep_num_ops_del_meta": "Metadata deletes per sec",
-    "couch_views_ops": "View reads per sec",
+    "xdc_ops": "Total XDCR operations per second",
+    "ep_num_ops_get_meta": "Metadata reads per second",
+    "ep_num_ops_set_meta": "Metadata sets per second",
+    "ep_num_ops_del_meta": "Metadata deletes per second",
+    "couch_views_ops": "View reads per second",
     "couch_views_data_size": "Views data size, bytes",
     "couch_views_actual_disk_size": "Views total disk size, bytes",
     "couch_views_fragmentation": "Views fragmentation, %",
@@ -81,24 +81,24 @@ LABELS = {
     "latency_observe": "OBSERVE latency, ms",
     "latency_persist_to": "persistTo=1 latency, ms",
     "latency_replicate_to": "replicateTo=1 latency, ms",
-    "data_rps": "Disk reads/sec (after merges)",
-    "data_wps": "Disk writes/sec (after merges)",
-    "data_rbps": "Bytes/sec read from the disk",
-    "data_wbps": "Bytes/sec written to the disk",
+    "data_rps": "Disk reads/second (after merges)",
+    "data_wps": "Disk writes/second (after merges)",
+    "data_rbps": "Bytes/second read from the disk",
+    "data_wbps": "Bytes/second written to the disk",
     "data_avgqusz": "The average queue length",
     "data_util": "Disk bandwidth utilization, %",
     "data_avg_page_cache_rr": "Average page cache resident ratio, %",
     'page_cache_hit_ratio': 'Page cache hit ratio, %',
     'page_cache_total_hits': 'Page cache total hits per second',
-    "index_rbps": "Bytes read/sec",
-    "index_wbps": "Bytes written/sec",
+    "index_rbps": "Bytes read/second",
+    "index_wbps": "Bytes written/second",
     "index_avgqusz": "The average queue length",
     "index_util": "Disk bandwidth utilization, %",
     "bucket_compaction_progress": "Compaction progress, %",
-    "in_bytes_per_sec": "Incoming bytes/sec",
-    "out_bytes_per_sec": "Outgoing bytes/sec",
-    "in_packets_per_sec": "Incoming packets/sec",
-    "out_packets_per_sec": "Outgoing packets/sec",
+    "in_bytes_per_sec": "Incoming bytes/second",
+    "out_bytes_per_sec": "Outgoing bytes/second",
+    "in_packets_per_sec": "Incoming packets/second",
+    "out_packets_per_sec": "Outgoing packets/second",
     "ESTABLISHED": "Connections in ESTABLISHED state",
     "TIME_WAIT": "Connections in TIME_WAIT state",
     "index_num_rows_returned": "Number of rows returned by 2i",
@@ -181,19 +181,19 @@ LABELS = {
     "mm_allocated": "Memory allocated, Indexer, bytes",
     "mm_resident": "Memory resident, Indexer, bytes",
     "mm_metadata": "Memory metadata, Indexer, bytes",
-    "query_requests": "Number of N1QL requests processed per sec",
-    "query_selects": "Number of N1QL selects processed per sec",
-    "query_avg_req_time": "Average end-to-end time to process a query, sec",
-    "query_avg_svc_time": "Average time to execute a query, sec",
+    "query_requests": "Number of N1QL requests processed per second",
+    "query_selects": "Number of N1QL selects processed per second",
+    "query_avg_req_time": "Average end-to-end time to process a query, seconds",
+    "query_avg_svc_time": "Average time to execute a query, seconds",
     "query_avg_response_size": "Average size of the data returned by a query, bytes",
     "query_avg_result_count": "Average number of results (documents) returned by a query",
-    "query_errors": "Number of N1QL errors returned per sec",
-    "query_warnings": "Number of N1QL errors returned per sec",
-    "query_requests_250ms": "Number of queries that take longer than 250ms per sec",
-    "query_requests_500ms": "Number of queries that take longer than 500ms per sec",
-    "query_requests_1000ms": "Number of queries that take longer than 1000ms per sec",
-    "query_requests_5000ms": "Number of queries that take longer than 5000ms per sec",
-    "query_invalid_requests": "Number of requests for unsupported endpoints per sec",
+    "query_errors": "Number of N1QL errors returned per second",
+    "query_warnings": "Number of N1QL errors returned per second",
+    "query_requests_250ms": "Number of queries that take longer than 250ms per second",
+    "query_requests_500ms": "Number of queries that take longer than 500ms per second",
+    "query_requests_1000ms": "Number of queries that take longer than 1000ms per second",
+    "query_requests_5000ms": "Number of queries that take longer than 5000ms per second",
+    "query_invalid_requests": "Number of requests for unsupported endpoints per second",
     "cbft_num_bytes_used_disk": "FTS index size on disk in GB",
     "cbft_doc_count": "FTS indexing rate",
     "cbft_num_bytes_used_ram": "FTS ram used by cbft process",
@@ -219,10 +219,10 @@ LABELS = {
     "elastic_filter_cache_size": "Elasticsearch filter cache size",
     "elastic_active_search": "Elasticsearch active search",
     "elastic_query_total": "Elasticsearch total query",
-    "indexer_pread": "indexer pread system calls/sec",
-    "indexer_pwrite": "indexer pwrite system calls/sec",
-    "memcached_pread": "memcached pread system calls/sec",
-    "memcached_pwrite": "memcached pwrite system calls/sec",
+    "indexer_pread": "indexer pread system calls/second",
+    "indexer_pwrite": "indexer pwrite system calls/second",
+    "memcached_pread": "memcached pread system calls/second",
+    "memcached_pwrite": "memcached pwrite system calls/second",
     'MemFree': 'The amount of physical memorynot used by the system, bytes',
     'Dirty': 'Memory waiting to be written back to disk, bytes',
     'Buffers': 'Memory in buffer cache, bytes',

commit b740b0f568d5786813fccb2f6e28eb5deac73dad

Generate unique filenames and links
This patch fixes the regression introduced in c49f155.

Change-Id: Ia10b698b6e36f8924e51b03816df8e949862ab17
Reviewed-on: http://review.couchbase.org/84275
Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

webapp/cbmonitor/plotter/__init__.py
@@ -1,5 +1,6 @@
 import os
 from itertools import cycle
+from hashlib import md5
 
 import matplotlib
 matplotlib.use('Agg')
@@ -130,10 +131,10 @@ def generate_title(observable):
         return metric
 
 
-def generate_paths(clusters, labels, metric, suffix):
+def generate_paths(clusters, labels, metric):
     """Generate file name and URL using the unique attributes."""
     sub_folder = ''.join(clusters + labels)
-    filename = "{}{}.png".format(metric, suffix)
+    filename = "{}.png".format(metric)
     path = os.path.join(sub_folder, filename)
 
     if not os.path.exists(os.path.join(settings.MEDIA_ROOT, sub_folder)):
@@ -291,8 +292,7 @@ class Plotter:
             for chart in generate_chart_types(metric):
                 url, filename = generate_paths(clusters=all_clusters,
                                                labels=custom_labels,
-                                               metric=metric,
-                                               suffix=chart)
+                                               metric=md5(title + chart).hexdigest())
 
                 images.append([title, url])
 